3195255de93e91f58f2a9129ce70e114a13a1a16
MadhushaPrasad <madushaprasad21@gmail.com>
2023-09-05 14:09:23 +0530

    chore (data file) : Create composable data files

---
+++ composables/courseData.js
@@ -0,0 +1,36 @@
+export default {
+    title: 'Course Data',
+    chapter: [
+        {
+            title: 'Chapter 1',
+            slug: '1-chapter-1',
+            number: 1,
+            lessons: [
+                {
+                    title: 'Introduction to TypeScript with vue.js',
+                    slug: '1-introduction-to-typescript-with-vuejs',
+                    number: 1,
+                    downloadUrl: 'https://vueschool.io/courses/typescript-with-vuejs',
+                    videoId: 452423423,
+                    text: "In this lesson, we'll learn about the benefits of using TypeScript with Vue.js and how to set up a Vue.js project with TypeScript."
+                },
+                {
+                    title: 'Introduction to TypeScript with vue.js',
+                    slug: '2-introduction-to-typescript-with-vuejs',
+                    number: 2,
+                    downloadUrl: 'https://vueschool.io/courses/typescript-with-vuejs',
+                    videoId: 452423423,
+                    text: "In this lesson, we'll learn about the benefits of using TypeScript with Vue.js and how to set up a Vue.js project with TypeScript."
+                },
+                {
+                    title: 'Introduction to TypeScript with vue.js',
+                    slug: '3-introduction-to-typescript-with-vuejs',
+                    number: 3,
+                    downloadUrl: 'https://vueschool.io/courses/typescript-with-vuejs',
+                    videoId: 452423423,
+                    text: "In this lesson, we'll learn about the benefits of using TypeScript with Vue.js and how to set up a Vue.js project with TypeScript."
+                }
+            ]
+        }
+    ]
+}
